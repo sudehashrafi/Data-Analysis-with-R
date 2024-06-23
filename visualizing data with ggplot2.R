df211f4fa61c2ab5f72e587828cee7228187208c
Soudeh <57491903+sudehashrafi@users.noreply.github.com>
2024-06-23 12:58:09 +1000

    Add files via upload
    Dataset: Palmer Penguins data
    
    plotting the relationship between body mass and flipper length in the three penguin species.

---
+++ visualizing data with ggplot2.R
@@ -0,0 +1,12 @@
+install.packages('tidyverse')
+install.packages('ggplot2')
+install.packages('palmerpenguins')
+
+library(tidyverse)
+library(ggplot2)
+library(palmerpenguins)
+
+data("penguins")
+View(penguins)
+
+ggplot(data = penguins) + geom_point(mapping = aes(x = flipper_length_mm, y = body_mass_g))
