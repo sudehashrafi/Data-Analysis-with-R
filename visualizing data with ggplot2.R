dceb09a35f6878ece8153bab210d60ac770cf150
Soudeh <57491903+sudehashrafi@users.noreply.github.com>
2024-06-23 13:21:08 +1000

    Update visualizing data with ggplot2.R

--- visualizing data with ggplot2.R
+++ visualizing data with ggplot2.R
@@ -9,4 +9,4 @@ library(palmerpenguins)
 data("penguins")
 View(penguins)
 
-ggplot(data = penguins) + geom_point(mapping = aes(x = flipper_length_mm, y = body_mass_g))
+ggplot(data = penguins) + geom_point(mapping = aes(x = flipper_length_mm, y = body_mass_g, color=species))
